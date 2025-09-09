cae101b1581cf1848aad5a38967e5ab57e683f3d
apple <ishwaryakathambari@gmail.com>
2025-09-09 12:18:29 +0530

    assignment 2

---
+++ constructor/src/constructor/main.java
@@ -0,0 +1,21 @@
+package constructor;
+
+public class main {
+
+	public static void main(String[] args) {
+		// Creating multiple students using constructor
+        Student s1 = new Student(1, "Ash", "Electronics and Communication");
+        Student s2 = new Student(2, "Ravi", "Computer Science");
+        Student s3 = new Student(3, "Priya", "Mechanical Engineering");
+
+        // Display student details
+        s1.displayInfo();
+        s2.displayInfo();
+        s3.displayInfo();
+    }
+
+		// TODO Auto-generated method stub
+
+	}
+
+
